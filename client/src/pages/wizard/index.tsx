fdd2e888fdbb2dea09c93f8d307323a7b2ff231c
Steve Smith <stevy@hey.com>
2025-02-02 10:16:41 +0000

    add template selection for new agent creation

--- client/src/pages/wizard/index.tsx
+++ client/src/pages/wizard/index.tsx
@@ -1,20 +1,20 @@
 import { Route, Switch } from "wouter";
 import { WizardLayout } from "@/components/wizard/WizardLayout";
 import { WizardProvider } from "@/hooks/use-wizard";
-import TemplateStep from "./template.tsx";
-import PersonaStep from "./persona.tsx";
-import PlatformStep from "./platform.tsx";
-import ReviewStep from "./review.tsx";
+import TemplateStep from "./template";
+import PersonaStep from "./persona";
+import PlatformStep from "./platform";
+import ReviewStep from "./review";
 
 export default function WizardPage() {
   return (
     <WizardProvider>
       <WizardLayout>
         <Switch>
-          <Route path="/" component={TemplateStep} />
-          <Route path="/persona" component={PersonaStep} />
-          <Route path="/platform" component={PlatformStep} />
-          <Route path="/review" component={ReviewStep} />
+          <Route path="/wizard" component={TemplateStep} />
+          <Route path="/wizard/persona" component={PersonaStep} />
+          <Route path="/wizard/platform" component={PlatformStep} />
+          <Route path="/wizard/review" component={ReviewStep} />
         </Switch>
       </WizardLayout>
     </WizardProvider>
